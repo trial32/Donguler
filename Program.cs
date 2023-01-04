47c01eb8e536fbe796d96bf1679adcef7e4c4210
icmosoftware <ozendeniz@gmail.com>
2023-01-04 13:28:28 +0300

    Modified

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+//FOR DÖNGÜLERİ
 int limit=100;
 int tekSayilar=0;
 int ciftSayilar=0;
@@ -15,4 +15,22 @@ for (int i = 0; i < limit; i++)
 
 Console.WriteLine( "Tek ayıların toplamı: " + tekSayilar);
 Console.WriteLine(  "Çift sayıların toplamı: " + ciftSayilar);
+//WHILE DÖNGÜSÜ
+
+char character='a';
+
+while (character<='z'){
+    Console.Write(character);
+    character++;
+}
+    Console.WriteLine("");
+//FOREACH DÖNGÜLERİ
+string [] arabalar ={"Ford","Totoya","Audi"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
+
+
 
