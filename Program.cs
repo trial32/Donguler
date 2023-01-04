ad74f343a0e9fb1c6fde67966dbefc312844f3dc
icmosoftware <ozendeniz@gmail.com>
2023-01-04 12:48:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int limit=100;
+int tekSayilar=0;
+int ciftSayilar=0;
+
+for (int i = 0; i < limit; i++)
+{
+    if (i % 2 == 0 )
+        ciftSayilar+=i;
+    else
+        tekSayilar+=i;
+}
+
+Console.WriteLine( "Tek ayıların toplamı: " + tekSayilar);
+Console.WriteLine(  "Çift sayıların toplamı: " + ciftSayilar);
+
